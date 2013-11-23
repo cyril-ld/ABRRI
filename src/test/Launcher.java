58023f963b577139a663badaf6dbcf3c1ed552c1
cyril-ld <cyril.ledriant@gmail.com>
2013-11-23 11:42:25 +0100

    Ajout classes de tests unitaires (JUnit 4)

--- src/test/Launcher.java
+++ src/test/Launcher.java
@@ -5,6 +5,7 @@ package test;
 
 import util.TreeUtils;
 import datastructure.BinaryTree;
+import datastructure.TreeNode;
 import exceptions.IntervalleInexistantException;
 
 /**
@@ -19,11 +20,15 @@ public class Launcher {
 	public static void main(String[] args) {
 
 		BinaryTree AABRI;
+		TreeNode node1, node2;
 
 		AABRI = (args.length == 2) ? TreeUtils.initBinaryTreeFromFile(args[1]) : TreeUtils.initBinaryTreeFromFile("resources/AABRI.txt");
 		// TODO : tester la suppression //
 		try {
-			System.out.println(AABRI.findTreeNode(AABRI.getRootNode(), 50, 75).getMin());
+			node1 = AABRI.findTreeNode(AABRI.getRootNode(), 50, 75);
+			System.out.println(node1.getMin());
+			node2 = AABRI.findTreeNode(AABRI.getRootNode(), 1, 2);
+			System.out.println(node2.getRoot());
 		} catch (IntervalleInexistantException e) {
 			e.printStackTrace();
 		}
---
+++ tests/datastructure/BinaryTreeTest.java
@@ -0,0 +1,120 @@
+/**
+ * 
+ */
+package datastructure;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Cyril
+ *
+ */
+public class BinaryTreeTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link datastructure.BinaryTree#insert(int, int, datastructure.SimpleNode)}.
+	 */
+	@Test
+	public void testInsertIntIntSimpleNode() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link datastructure.BinaryTree#insert(datastructure.TreeNode)}.
+	 */
+	@Test
+	public void testInsertTreeNode() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link datastructure.BinaryTree#findTreeNode(datastructure.TreeNode, int, int)}.
+	 */
+	@Test
+	public void testFindTreeNode() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link datastructure.BinaryTree#findTreeNodeFromValue(datastructure.TreeNode, int)}.
+	 */
+	@Test
+	public void testFindTreeNodeFromValue() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link datastructure.BinaryTree#delete(int, int)}.
+	 */
+	@Test
+	public void testDelete() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link datastructure.BinaryTree#addSimpleNode(datastructure.SimpleNode)}.
+	 */
+	@Test
+	public void testAddSimpleNodeSimpleNode() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link datastructure.BinaryTree#addSimpleNode(int)}.
+	 */
+	@Test
+	public void testAddSimpleNodeInt() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link datastructure.BinaryTree#removeSimpleNode(int)}.
+	 */
+	@Test
+	public void testRemoveSimpleNode() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link datastructure.BinaryTree#ABRtoAABRI()}.
+	 */
+	@Test
+	public void testABRtoAABRI() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link datastructure.BinaryTree#AABRItoABR()}.
+	 */
+	@Test
+	public void testAABRItoABR() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link datastructure.BinaryTree#getRootNode()}.
+	 */
+	@Test
+	public void testGetRootNode() {
+		fail("Not yet implemented");
+	}
+
+}
